cf4a45aec67a22d19ed68ece6c96b9915fd8390a
Manav Khandurie <manavkhandurie@gmail.com>
2023-09-11 02:24:07 +0530

    q237

---
+++ q237.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode head) {
+        //[4,5,1,9]
+        //   i j
+        //[4,1,1,9]
+        //     i j
+        //[4,1,9,9]
+        //       i j
+
+        ListNode curr=head.next;
+        while(curr.next!=null){
+            head.val=curr.val;
+            head=head.next;
+            curr=curr.next;
+        }
+        head.val=curr.val;
+        head.next=null;
+    }
+}
