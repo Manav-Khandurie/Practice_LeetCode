a0799d745310fee02e645c4e35b9cedc9b8f9b99
Manav Khandurie <manavkhandurie@gmail.com>
2023-09-15 11:26:21 +0530

    added q6

---
+++ q6.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String convert(String s, int n) {
+        if(n==1)
+            return s;
+        String []arr=new String[n];
+        int k=0,m=s.length(); boolean flag=true;
+        for(int x=0;x<m;x++){
+            if(arr[k]==null)    arr[k]="";
+            arr[k]+=s.charAt(x);
+            //System.out.println(arr[k]);
+            if(flag)    k++;
+            else        k--;
+
+            if(k>=n){
+                flag=false;
+                k=n-2;
+            }else if(k<0){
+                flag=true;
+                k=1;
+            }
+        }
+
+        String ans="";
+        for(int x=0;x<arr.length;x++){
+            if(arr[x]!=null)
+                ans+=arr[x];
+        }
+        return ans;
+    }
+}
