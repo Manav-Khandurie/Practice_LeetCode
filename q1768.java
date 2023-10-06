4e9a0fa970161e1ada15a8dc4cbab399b9d2eca1
Manav Khandurie <manavkhandurie@gmail.com>
2023-10-06 09:57:03 +0530

    updated q1768

--- q1768.java
+++ q1768.java
@@ -1,4 +1,4 @@
-class q1768 {
+class Solution {
     public String mergeAlternately(String word1, String word2) {
         int n=word1.length(),m=word2.length();
 
@@ -25,3 +25,41 @@ class q1768 {
         return s3;
     }
 }
+/*
+q1768 Merge Strings Alternately
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+
+
+Java
+Runtime
+7 ms
+Beats
+25.61%
+Memory
+42.2 MB
+Beats
+6.48%
+
+*/
