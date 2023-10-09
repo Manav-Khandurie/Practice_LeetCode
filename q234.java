0be2d15056730edce3386e72594b97d60c9a2c7d
Manav Khandurie <manavkhandurie@gmail.com>
2023-10-09 16:55:08 +0530

    added q234

--- q234.java
+++ q234.java
@@ -52,3 +52,44 @@ class q234 {
         return true;
     }
 }
+/*
+Q234 Palindrome Linked List
+
+Given the head of a singly linked list, return true if it is a 
+palindrome
+ or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+
+Follow up: Could you do it in O(n) time and O(1) space?
+
+ */
+/*
+Runtime
+Details
+3ms
+Beats 100.00%of users with Java
+
+Memory
+Details
+56.60MB
+Beats 64.28%of users with Java
+ */
