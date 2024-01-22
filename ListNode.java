57e2b0fef91629abbed4238ab296a901e7b548d9
Manav Khandurie <manavkhandurie@gmail.com>
2024-01-23 01:15:03 +0530

    ListNode

--- ListNode.java
+++ ListNode.java
@@ -1,9 +1,18 @@
-
 public class ListNode {
     int val;
     ListNode next;
+
+    ListNode() {
+
+    }
+
     ListNode(int x) {
         val = x;
         next = null;
+    }
+
+    ListNode(int x, ListNode t) {
+        val = x;
+        next = t;
     }
 }
