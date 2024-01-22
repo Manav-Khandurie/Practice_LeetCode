860a0044528f081ef1ee55e66a4cc59ddbaf4ed6
Manav Khandurie <manavkhandurie@gmail.com>
2024-01-22 17:00:57 +0530

    added q645

---
+++ q645.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class q645 {
+    public int[] findErrorNums(int[] nums) {
+        
+        Set<Integer> hs=new HashSet<>();
+        int isRepeated=0,isMissing=0;
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i]))
+                isRepeated=nums[i];
+            hs.add(nums[i]);
+        }
+        // We have the repeted term
+        int n=nums.length;
+        for(int i=1;i<=n;i++){
+            if(!hs.contains(i))
+                isMissing=i;
+        }
+        int ans[]={isRepeated,isMissing};
+        return ans;
+    }
+}
+/*
+Q645. Set Mismatch
+
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+You are given an integer array nums representing the data status of this set after the error.
+
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,4]
+Output: [2,3]
+Example 2:
+
+Input: nums = [1,1]
+Output: [1,2]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+1 <= nums[i] <= 104
+*/
+/*
+Runtime
+15ms Beats 11.03% of users with Java
+
+Memory
+45.84MB Beats 11.88% of users with Java
+*/
