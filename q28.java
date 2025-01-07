4e342f5de650005de9fa0d1e4cb1e5eb18cd8924
Manav Khandurie <manavkhandurie@gmail.com>
2025-01-08 01:59:42 +0530

    updated q28

--- q28.java
+++ q28.java
@@ -1,32 +1,47 @@
 class q28 {
-    public int strStr(String haystack, String needle) {
-        // int cap=needle.length();
-        // int n=haystack.length();
-        // int i=0;
-        return haystack.indexOf(needle);
+
+    private static void getZarr(String str, int[] Z) {
+        int n = str.length();
+        int L = 0, R = 0;
+        for (int i = 1; i < n; ++i) {
+            if (i > R) {
+                L = R = i;
+                while (R < n && str.charAt(R - L) == str.charAt(R)) {
+                    R++;
+                }
+                Z[i] = R - L;
+                R--;
+            } else {
+                int k = i - L;
+                if (Z[k] < R - i + 1) {
+                    Z[i] = Z[k];
+                } else {
+                    L = i;
+                    while (R < n && str.charAt(R - L) == str.charAt(R)) {
+                        R++;
+                    }
+                    Z[i] = R - L;
+                    R--;
+                }
+            }
+        }
     }
-}/***
-Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
- 
-
-Example 1:
-
-Input: haystack = "sadbutsad", needle = "sad"
-Output: 0
-Explanation: "sad" occurs at index 0 and 6.
-The first occurrence is at index 0, so we return 0.
-Example 2:
-
-Input: haystack = "leetcode", needle = "leeto"
-Output: -1
-Explanation: "leeto" did not occur in "leetcode", so we return -1.
- 
-
-Constraints:
-
-1 <= haystack.length, needle.length <= 104
-haystack and needle consist of only lowercase English characters.
-
-
-*/
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+        String combined = needle + "#" + haystack;
+        int[] z = new int[combined.length()];
+        getZarr(combined, z);
+        for (int i = needle.length() + 1; i < z.length; i++) {
+            if (z[i] == needle.length()) {
+                return i - (needle.length() + 1);
+            }
+        }
+        return -1;
+    }
+}
